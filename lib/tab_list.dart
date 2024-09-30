b9207d5c720c529a7cf8b5cdb8f9c72777302e6b
Ali <113106857+yesAlima@users.noreply.github.com>
2024-09-30 17:44:25 +0300

    ignore warnings

--- lib/tab_list.dart
+++ lib/tab_list.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: library_private_types_in_public_api, duplicate_ignore
+
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'laptop_details.dart';
 import 'tab_new.dart';
--- lib/utils/texfield.dart
+++ lib/utils/texfield.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: library_private_types_in_public_api, camel_case_types
+
 import 'package:flutter/material.dart';
 
 class reusableTextField extends StatefulWidget {
